65652c4bd9b68c6fdb6bd0757fd9127c663247a2
Manuel Alva <barrera_ml@hotmail.com>
2024-03-21 01:33:41 -0600

    Fixed Alerts

--- RenoWebApp/Components/Pages/Query.razor.cs
+++ RenoWebApp/Components/Pages/Query.razor.cs
@@ -20,6 +20,8 @@ namespace RenoWebApp.Components.Pages
         public List<Supply> Supplies { get; set; }
 
         public string jobType {get; set; }
+        public List<string> errorList = new List<string>(); 
+        public string feedback { get; set; }
 
         [Parameter]
         public int JobId { get; set; }
@@ -38,16 +40,31 @@ namespace RenoWebApp.Components.Pages
 
         private void HandleSelectedJob()
         {
-            if (JobId !=0)
+            if (JobId != 0)
             {
                 Supplies = SupplyServices.GetSuppliesByJobId(JobId);
                 NavigationManager.NavigateTo($"/query/{JobId}");
+
+                feedback = "View query results: ";
+                errorList.Clear();
+            }
+            else
+            {
+                feedback = "You must select a job to view supplies";
+                errorList.Add(feedback);    
             }
         }  
         
         private void ClearFields()
         {
-            JobId = 0;  
+            JobId = 0;              
+            feedback = "";
+            errorList.Clear();
+
+            if (Supplies != null)
+            {
+                Supplies.Clear();   
+            }
         }
     }
 }
